9db51c5951969ee830b51a954442271db3b6b0e1
Alejandro Zambrano <alejozd79@gmail.com>
2026-01-06 11:21:25 -0500

    faltaba la ruta

--- src/routes/serialesRoutes.js
+++ src/routes/serialesRoutes.js
@@ -9,5 +9,6 @@ router.get("/", serialesController.getAll);
 router.get("/cliente/:clienteId", serialesController.getByCliente);
 router.post("/", serialesController.create);
 router.delete("/:id", serialesController.delete);
+router.put("/:id", serialesController.update);
 
 module.exports = router;
